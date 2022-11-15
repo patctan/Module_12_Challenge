aa528b00c1328ebfddfd822838090059031973f1
Bitaw <stickynotes5@yahoo.com>
2022-11-15 14:36:43 -0600

    Added inquirer prompt to index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-const db = mysql.createConnection('mysql://root:rootroot@localhost:3301/tracker_db')
+const mysql = require('mysql2');
+const db = mysql.createConnection('mysql://root:rootroot@localhost:3301/tracker_db');
+const inquirer = require('inquirer');
+
+
+inquirer.prompt([{
+    message: 'What would you like to do?',
+    type: 'list',
+choices: ['Add department', 'Add role', 'Add employee', 'View departments', 'View roles', 'View employees', 'Update employee role'],
+name: 'init'
+}
+])
+.then(init=> {
+    console.log(init)
+})
