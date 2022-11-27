08abd464177f75e4ff618d4c9ac2ea987daecbef
Bitaw <stickynotes5@yahoo.com>
2022-11-27 00:13:55 -0600

    Created a seeds.sql file

---
+++ seeds.sql
@@ -0,0 +1,15 @@
+INSERT INTO department (name)
+VALUES ('Music'),
+       ('Literature'),
+       ('Science'),
+       ('Art'),
+       ('Math');
+
+INSERT INTO roles (title, salary)
+VALUES ('Teacher', 42000),
+       ('Teaching Assistant', '27000'),
+       ('Band Director', 50000),
+       ('Professor', 70000);
+
+INSERT INTO employee (first_name, last_name)
+    
