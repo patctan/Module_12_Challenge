6fc2168183db56d196cc4857fa18d5c71e900f10
Bitaw <stickynotes5@yahoo.com>
2022-11-27 14:09:06 -0600

    Added id numbers to seeds.sql

--- seeds.sql
+++ seeds.sql
@@ -5,19 +5,19 @@ VALUES ('Music'),
        ('Art'),
        ('Math');
 
-INSERT INTO roles (title, salary)
-VALUES ('Teacher', 42000),
-       ('Teaching Assistant', '27000'),
-       ('Band Director', 50000),
-       ('Professor', 70000);
+INSERT INTO roles (title, salary, department_id)
+VALUES ('Teacher', 42000, 1),
+       ('Teaching Assistant', 27000, 2),
+       ('Band Director', 50000, 3),
+       ('Professor', 70000, 4);
 
-INSERT INTO employee (first_name, last_name)
-VALUES ('Taeyeon', 'Kim'),
-       ('Jessica', 'Jung'),
-       ('Sooyoung', 'Choi'),
-       ('Yuri', 'Kwon'),
-       ('Hyoyeon', 'Kim'),
-       ('Sunny', 'Lee'),
-       ('Yoona', 'Im'),
-       ('Juhyun', 'Seo'),
-       ('Tiffany', 'Young');
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Taeyeon', 'Kim', 1, 1),
+       ('Jessica', 'Jung', 2, 2),
+       ('Sooyoung', 'Choi', 3, 3),
+       ('Yuri', 'Kwon', 4, 4),
+       ('Hyoyeon', 'Kim', 5, 5),
+       ('Sunny', 'Lee', 6, 6),
+       ('Yoona', 'Im', 7, 7),
+       ('Juhyun', 'Seo', 8, 8),
+       ('Tiffany', 'Young', 9, 9);
